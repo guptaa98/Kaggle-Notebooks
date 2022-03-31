16cc09bdaffc60dec42f187d7b722b7f67b73063
Prashasti Gupta <prashasti708@gmail.com>
2022-03-31 17:26:41 +0530

    Add files via upload

--- Sales Profit Prediction/App.py
+++ Sales Profit Prediction/App.py
@@ -3,22 +3,29 @@ import pandas as pd
 import os
 from flask import Flask, request, render_template
 import pickle
+from sklearn import preprocessing   
+label_encoder = preprocessing.LabelEncoder()
+
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler() 
 
 app = Flask(__name__)
 MODEL_PATH = 'salesprofit_predictionmodel.pkl'
 model = pickle.load(open(MODEL_PATH, 'rb'))
 df = pd.read_csv("Sales_project_phaseII.csv")
+df1 = pd.read_csv('50000 Sales Records.csv')
+
+Order_Priority_dict = {'L':1, 'M':2, 'H':3, 'C':4}
+Item_Type_dict = {'Meat':1,'Fruits':2,'Cosmetics':3,'Vegetables':4,'Personal Care':5,'Beverages':6,'Snacks':7,
+                 'Clothes':8, 'Cereal':9, 'Household':0, 'Office Supplies':10 , 'Baby Food':11}
+Sales_Channel_dict = {'Online':1 , 'Offline':0}
+Region_dict = {'Sub-Saharan Africa':0,'Europe':1,'Asia':2,'Middle East and North Africa':3,
+              'Central America and the Caribbean':4,'Australia and Oceania':5,'North America':6}
 
 
-from sklearn import preprocessing   
-label_encoder = preprocessing.LabelEncoder()
+
 df['Country']= label_encoder.fit_transform(df['Country'])
-
-from sklearn.preprocessing import MinMaxScaler
-scaler = MinMaxScaler() 
 df[['Region','Country','Item Type','Sales Channel','Order Priority','Total Revenue','Total Cost','Units Sold','Unit Price','Unit Cost','Order Date_year','Order Date_month','Order Date_day','Ship Date_year','Ship Date_month','Ship Date_day']] = scaler.fit_transform(df[['Region','Country','Item Type','Sales Channel','Order Priority','Total Revenue','Total Cost','Units Sold','Unit Price','Unit Cost','Order Date_year','Order Date_month','Order Date_day','Ship Date_year','Ship Date_month','Ship Date_day']])
- #= scaler.fit_transform(df[['Units Sold','Unit Price','Unit Cost','Order Date_year','Order Date_month','Order Date_day','Ship Date_year','Ship Date_month','Ship Date_day']])
-
 
 @app.route('/')
 def home():
@@ -35,15 +42,21 @@ def predict():
         # int_features = [int(x) for x in request.form.values()]
         features = [x for x in request.form.values()]
         # print(features)
-        if(features[4] == 'H'):
-            features[4] = 3
-        else:
-            if(features[4] == 'M'):
-                features[4] = 2
-            else:
-                features[4] = 1
-         
+        features[4] = Order_Priority_dict[features[4]]
+        features[0] = Region_dict[features[0]]
         features[1] = label_encoder.transform([features[1]])
+        features[2] = Item_Type_dict[features[2]]
+        features[3] = Sales_Channel_dict[features[3]]
+
+        # if(features[4] == 'H'):
+        #     features[4] = 3
+        # else:
+        #     if(features[4] == 'M'):
+        #         features[4] = 2
+        #     else:
+        #         features[4] = 1
+         
+        
         # print(features)
         # for i in range(len(features)):
         #     if(i==1):
